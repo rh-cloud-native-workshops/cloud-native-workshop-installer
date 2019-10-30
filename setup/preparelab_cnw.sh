68f40b27be3279160872372a5192f7ed76fcaa6b
cvicens <carlos.vicens.alonso@gmail.com>
2019-10-30 19:48:15 +0100

    etherpad added

--- setup/preparelab_cnw.sh
+++ setup/preparelab_cnw.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 MYDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 function usage() {
-    echo "usage: $(basename $0) [-c/--count usercount] -n/--namespace infra-namespace -m/--monitoring"
+    echo "usage: $(basename $0) [-c/--count usercount] -n/--namespace infra-namespace -m/--monitoring -e/--etherpad"
 }
 
 # Defaults
 USER_COUNT=10
 INFRA_NAMESPACE="lab-infra"
 MONITORING_NAMESPACE="lab-monitoring"
+ETHERPAD_NAMESPACE="lab-etherpad"
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -30,6 +31,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -e|--etherpad)
+    ETHERPAD_NAMESPACE="$2"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     echo "Unknown option: $key"
     usage
@@ -44,9 +50,10 @@ oc new-project ${INFRA_NAMESPACE}
 #printf "[defaults]\nroles_path = ${ROLES_PATH}" > ansible.cfg
 #export ANSIBLE_CONFIG=./ansible.cfg
 
-ansible-playbook -vvv playbooks/provision-monitoring.yml \
+ansible-playbook -vvv playbooks/provision.yml \
     -e infra_namespace=${INFRA_NAMESPACE} \
     -e monitoring_namespace=${MONITORING_NAMESPACE} \
+    -e etherpad_namespace=${ETHERPAD_NAMESPACE} \
     -e openshift_token=$(oc whoami -t) \
     -e openshift_master_url=$(oc whoami --show-server) \
     -e openshift_user_password='openshift' \
