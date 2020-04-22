ebc6e6a3e0187bf598f5a24b85ac60997bfae581
cvicens <carlos.vicens.alonso@gmail.com>
2020-04-22 19:03:43 +0200

    image ocp-4.3

--- build-image.sh
+++ build-image.sh
@@ -3,7 +3,7 @@
 export REGISTRY=quay.io
 export REGISTRY_USER_ID=cvicensa
 export IMAGE_NAME=cnw-installer
-export IMAGE_VERSION=v0.0.2
+export IMAGE_VERSION=ocp-4.3
 
 docker build -t $REGISTRY/$REGISTRY_USER_ID/$IMAGE_NAME:$IMAGE_VERSION .
 
